1f7654ac56c1358f85201f96617dc28885569480
Nick Garver <nickgarver5@gmail.com>
2018-06-03 23:10:08 -0400

    init
    added cheats lol

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 var game = false;
 var number = null;
-var temp = null;
+var numberTwo = null;
 var score = null;
 
   // background: #fab1a0; /* lose */
@@ -9,38 +9,40 @@ var score = null;
 
 
 function beginGame(){
-  number = parseInt(Math.random()*100); 
+  number = parseInt(Math.random()*100);
+  numberTwo = parseInt(Math.random()*100);
   game = true;
   score = 0;
   document.getElementById("number").classList.remove('number-flash');
-  document.getElementById("number").innerHTML = number; 
-  document.getElementById("directions").style.display = "none"; 
+  document.getElementById("number").innerHTML = number;
+  document.getElementById("directions").style.display = "none";
   document.getElementById("resultOuter").className = "bottom-screen";
-  document.getElementById("score").innerHTML = 'Score: '+ score; 
+  document.getElementById("score").innerHTML = 'Score: '+ score;
   refreshData('null')
 }
 
 function choiceBtn(choice){
   if(game == false)
     return;
-  
-  temp = parseInt(Math.random()*100);
+
   document.getElementById("number").innerHTML = choice;
-  if((choice == 'lower' & temp < number) || (choice == 'higher' & temp > number) || (choice == 'same' & temp == number)){
+  if((choice == 'lower' & numberTwo < number) || (choice == 'higher' & numberTwo > number) || (choice == 'same' & numberTwo == number)){
     document.getElementById("resultOuter").className = '';
     void document.getElementById("resultOuter").offsetWidth;
+    number = numberTwo;
+    numberTwo = parseInt(Math.random()*100);
     refreshData(choice)
-    number = temp;
     score++
-    document.getElementById("number").innerHTML = number; 
-    document.getElementById("result").innerHTML = 'Correct!'; 
+    document.getElementById("number").innerHTML = number;
+    document.getElementById("result").innerHTML = 'Correct!';
     document.getElementById("resultOuter").className = 'bottom-screen win';
     document.getElementById("score").innerHTML = 'Score: '+ score;
   } else {
+    numberTwo = parseInt(Math.random()*100);
     refreshData(choice)
     document.getElementById("result").innerHTML = 'Game Over!';
     document.getElementById("resultOuter").className = 'bottom-screen lose';
-    document.getElementById("number").innerHTML = 'Nope, '+ temp +'!....Press Start to play again.'; 
+    document.getElementById("number").innerHTML = 'Nope, '+ numberTwo +'!....Press Start to play again.';
     score = 0;
     game = false;
   }
@@ -53,6 +55,12 @@ function toggleCheat(){
 
 function refreshData(choice){
   document.getElementById("old").innerHTML = 'Old Num: ' + number;
-  document.getElementById("new").innerHTML = 'New Num: ' + temp; 
-  document.getElementById("guess").innerHTML = 'Pick: ' + choice;  
+  document.getElementById("new").innerHTML = 'New Num: ' + numberTwo;
+  if (numberTwo < number) {
+    document.getElementById("guess").innerHTML = 'Pick: lower';
+  } else if (numberTwo > number) {
+      document.getElementById("guess").innerHTML = 'Pick: higher';
+  } else if (numberTwo == number) {
+      document.getElementById("guess").innerHTML = 'Pick: same';
+  }
 }
